aec1c2bb8e28a95a8210a3964f48961791710410
Hazem <hazem.badr167@gmail.com>
2023-10-06 18:13:44 +0300

    set data from api to page live

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ const apikey="812b74977262499f24cfe950d325461f";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 async function checkweather(){
     const response= await fetch(apiUrl + `&appid=${apikey}`);
+    let jsondata=await response.json();
+    console.log(jsondata);
+    document.querySelector(".city").innerHTML=jsondata.name;
+    document.querySelector(".temp").innerHTML=Math.round(jsondata.main.temp)  + "°C";
+    document.querySelector(".humidity").innerHTML=jsondata.main.humidity +"%";
+    document.querySelector(".Wind").innerHTML=jsondata.wind.speed + " km/h";
+
 }
+checkweather();
