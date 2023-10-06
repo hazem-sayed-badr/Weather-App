90ea8bc4d39b44538be03b1d9bce563749f08097
Hazem <hazem.badr167@gmail.com>
2023-10-06 21:17:50 +0300

    Last Version With Editing And The Final

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ const apikey="812b74977262499f24cfe950d325461f";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 let searchfield=document.querySelector(".search input");
 let searchbutton=document.querySelector(".search button");
-let weathericon=document.querySelector(".weather-icon")
+let weathericon=document.querySelector(".weather-icon");
+let weathersection=document.querySelector(".weather");
+let errormsg=document.querySelector(".error");
+let details=document.querySelector(".details");
+let title=document.querySelector(".title");
 async function checkweather(city){
     const response= await fetch(apiUrl + city +  `&appid=${apikey}`);
-    let jsondata=await response.json();
+    if(response.status===404){
+        errormsg.style.display="block";
+        weathersection.style.display="none";
+    }
+    else{
+        title.style.display="none";
+        let jsondata=await response.json();
     console.log(jsondata);
     document.querySelector(".city").innerHTML=jsondata.name;
     document.querySelector(".temp").innerHTML=Math.round(jsondata.main.temp)  + "°C";
@@ -26,10 +36,14 @@ async function checkweather(city){
     else if(jsondata.weather[0].main==="Mist"){
         weathericon.src="images/mist.png";
     }
-
+ 
+        details.style.display="flex";
+    }
+      
 
 }
 searchbutton.onclick=function(){
+    weathersection.style.display="block";
     let value=searchfield.value;
     if(value===""){
         window.location.reload();
