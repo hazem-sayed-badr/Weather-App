f54911ad1da736c98a1fb643d5e91653a9ea7793
Hazem <hazem.badr167@gmail.com>
2023-10-06 19:59:50 +0300

    The Last Version And The End

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const apikey="812b74977262499f24cfe950d325461f";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 let searchfield=document.querySelector(".search input");
 let searchbutton=document.querySelector(".search button");
+let weathericon=document.querySelector(".weather-icon")
 async function checkweather(city){
     const response= await fetch(apiUrl + city +  `&appid=${apikey}`);
     let jsondata=await response.json();
@@ -10,6 +11,22 @@ async function checkweather(city){
     document.querySelector(".temp").innerHTML=Math.round(jsondata.main.temp)  + "°C";
     document.querySelector(".humidity").innerHTML=jsondata.main.humidity +"%";
     document.querySelector(".Wind").innerHTML=jsondata.wind.speed + " km/h";
+    if(jsondata.weather[0].main==="Clouds"){
+        weathericon.src="images/clouds.png";
+    }
+    else if(jsondata.weather[0].main==="Clear"){
+        weathericon.src="images/clear.png";
+    }
+    else if(jsondata.weather[0].main==="Rain"){
+        weathericon.src="images/rain.png";
+    }
+    else if(jsondata.weather[0].main==="Drizzle"){
+        weathericon.src="images/drizzle.png";
+    }
+    else if(jsondata.weather[0].main==="Mist"){
+        weathericon.src="images/mist.png";
+    }
+
 
 }
 searchbutton.onclick=function(){
