3d3d29fa590fbc5883fbb2bbb8f67d3c25060f87
Hazem <hazem.badr167@gmail.com>
2023-10-06 05:27:57 +0300

    fetch data by api url & key

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 const apikey="812b74977262499f24cfe950d325461f";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+async function checkweather(){
+    const response= await fetch(apiUrl + `&appid=${apikey}`);
+}
